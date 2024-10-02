b2a2fb95fe82cbe429d40acb27d919804b7f0cf2
Khaled Hegazy <khaledhegazy870@gmail.com>
2024-10-03 02:34:53 +0300

    remove all crud operations other than creating

--- chats/main.go
+++ chats/main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"encoding/json"
 	_ "fmt"
 	"net/http"
 	"os"
 	"strconv"
 
-	// "strconv"
-
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -33,40 +29,8 @@ type queueMessage struct {
 	Data      interface{} `json:"data"`
 }
 
-// Database connection pool
-var db *sql.DB
+// Redis Client
 var redis_client *redis.Client
-
-func getChats(c *gin.Context) {
-	rows, err := db.Query("SELECT chats.number, applications.token FROM applications join chats on chats.application_id = applications.id")
-
-	if err != nil {
-		print(err.Error())
-		return
-	}
-	defer rows.Close()
-
-	chats := []chat{}
-
-	// Iterate through the result set and scan into chat structs
-	for rows.Next() {
-		var chat chat
-		if err := rows.Scan(&chat.Number, &chat.ApplicationToken); err != nil {
-			print(err.Error())
-			return
-		}
-		chats = append(chats, chat)
-	}
-
-	// Check for errors from iterating over rows.
-	if err := rows.Err(); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Return the result as JSON
-	c.IndentedJSON(http.StatusOK, chats)
-}
 
 func createChat(c *gin.Context) {
 	var newChat chat
@@ -76,6 +40,29 @@ func createChat(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	application_token := newChat.ApplicationToken
+	application_name_in_hash := "app#" + application_token
+
+	// Check if the application is already created
+	// if created it will have a the application token will exists in applications_chats_count hash
+	exists, err := redis_client.HExists(c, "applications_chats_count", application_name_in_hash).Result()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to check existence for application"})
+		return
+	}
+	if !exists {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Application is not found"})
+		return
+	}
+
+	// Atomically Increment The Chats Number Per Application and get the total count (chat number)
+	chats_number_result, err := redis_client.HIncrBy(c, "applications_chats_count", application_name_in_hash, 1).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	newChat.Number = int(chats_number_result)
 
 	// Create a queue message with the operation type "create"
 	queueData := queueMessage{
@@ -92,7 +79,7 @@ func createChat(c *gin.Context) {
 
 	// Push the serialized chat data onto a Redis list (queue)
 	ctx := context.Background()
-	err = redis_client.RPush(ctx, "test_chats_queue", queueMessageData).Err()
+	err = redis_client.RPush(ctx, "chats_queue", queueMessageData).Err()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to push chat to Redis queue"})
 		return
@@ -100,127 +87,6 @@ func createChat(c *gin.Context) {
 
 	// Return the created chat data immediately
 	c.JSON(http.StatusCreated, newChat)
-}
-func updateChat(c *gin.Context) {
-	// Get the application_token and chat_number from the URL parameters
-	applicationToken := c.Param("application_token")
-	chatNumber := c.Param("chat_number")
-
-	// Parse the JSON request body into the chat struct
-	var updatedChat chat
-	if err := c.ShouldBindJSON(&updatedChat); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Set the application_token and chat_number from the URL params
-	updatedChat.ApplicationToken = applicationToken
-
-	// Convert chat_number to int
-	chatNum, err := strconv.Atoi(chatNumber)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid chat number"})
-		return
-	}
-	updatedChat.Number = chatNum
-
-	// Create a queue message with the operation type "update"
-	queueData := queueMessage{
-		Operation: "update", // Set the operation type to update
-		Data:      updatedChat,
-	}
-
-	// Serialize the queue message to JSON
-	queueMessageData, err := json.Marshal(queueData)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to serialize chat data"})
-		return
-	}
-
-	// Push the serialized chat data onto a Redis list (queue)
-	ctx := context.Background()
-	err = redis_client.RPush(ctx, "test_chats_queue", queueMessageData).Err()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to push chat to Redis queue"})
-		return
-	}
-
-	// Return the updated chat data immediately
-	c.JSON(http.StatusOK, updatedChat)
-}
-
-func deleteChat(c *gin.Context) {
-	// Get the application_token and chat_number from the URL parameters
-	applicationToken := c.Param("application_token")
-	chatNumber := c.Param("chat_number")
-
-	// Create a chat struct using the provided parameters
-	var chatToDelete chat
-	chatToDelete.ApplicationToken = applicationToken
-
-	// Convert chat_number to int
-	chatNum, err := strconv.Atoi(chatNumber)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid chat number"})
-		return
-	}
-	chatToDelete.Number = chatNum
-
-	// Create a queue message with the operation type "delete"
-	queueData := queueMessage{
-		Operation: "delete", // Set the operation type to delete
-		Data:      chatToDelete,
-	}
-
-	// Serialize the queue message to JSON
-	queueMessageData, err := json.Marshal(queueData)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to serialize chat data"})
-		return
-	}
-
-	// Push the serialized chat data onto a Redis list (queue)
-	ctx := context.Background()
-	err = redis_client.RPush(ctx, "test_chats_queue", queueMessageData).Err()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to push chat to Redis queue"})
-		return
-	}
-
-	// Return a confirmation message
-	c.JSON(http.StatusOK, gin.H{"status": "Chat delete request added to queue"})
-}
-
-func getMessages(c *gin.Context) {
-	// Prepare SQL query
-	rows, err := db.Query("SELECT messages.number, chats.number, applications.token,messages.content FROM applications join chats on chats.application_id = applications.id join messages on chats.id = messages.chat_id")
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	defer rows.Close()
-
-	messages := []message{}
-
-	// Iterate through the result set and scan into message structs
-	for rows.Next() {
-		var msg message
-		if err := rows.Scan(&msg.Number, &msg.ChatNumber, &msg.ApplicationToken, &msg.Content); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		messages = append(messages, msg)
-	}
-
-	// Check for errors from iterating over rows.
-	if err := rows.Err(); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Return the result as JSON
-	c.IndentedJSON(http.StatusOK, messages)
 }
 
 func createMessage(c *gin.Context) {
@@ -231,6 +97,30 @@ func createMessage(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	application_token := newMessage.ApplicationToken
+	chat_number := newMessage.ChatNumber
+	chat_name_in_hash := "chat#" + application_token + "-" + strconv.Itoa(chat_number)
+
+	// Check if the chat is already created
+	// if created it will have a the chat_name will exists in chats_messages_count hash
+	exists, err := redis_client.HExists(c, "chats_messages_count", chat_name_in_hash).Result()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to check existence for chat"})
+		return
+	}
+	if !exists {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Chat is not found"})
+		return
+	}
+
+	// Atomically Increment The Messages Number Per Chat and get the total count (message number)
+	messages_number_result, err := redis_client.HIncrBy(c, "chats_messages_count", chat_name_in_hash, 1).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	newMessage.Number = int(messages_number_result)
 
 	// Create a queue message with the operation type "create"
 	queueData := queueMessage{
@@ -247,7 +137,7 @@ func createMessage(c *gin.Context) {
 
 	// Push the serialized message data onto a Redis list (queue)
 	ctx := context.Background()
-	err = redis_client.RPush(ctx, "test_messages_queue", queueMessageData).Err()
+	err = redis_client.RPush(ctx, "messages_queue", queueMessageData).Err()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to push message to Redis queue"})
 		return
@@ -255,113 +145,6 @@ func createMessage(c *gin.Context) {
 
 	// Return the created message data immediately
 	c.JSON(http.StatusCreated, newMessage)
-}
-
-func updateMessage(c *gin.Context) {
-	// Get the application_token and chat_number from the URL parameters
-	applicationToken := c.Param("application_token")
-	chatNumber := c.Param("chat_number")
-	messageNumber := c.Param("message_number")
-
-	// Parse the JSON request body into the message struct
-	var updatedMessage message
-	if err := c.ShouldBindJSON(&updatedMessage); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Set the application_token and chat_number from the URL params
-	updatedMessage.ApplicationToken = applicationToken
-
-	// Convert chat_number to int
-	chatNum, err := strconv.Atoi(chatNumber)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid chat number"})
-		return
-	}
-	updatedMessage.ChatNumber = chatNum
-
-	// Convert chat_number to int
-	messageNum, err := strconv.Atoi(messageNumber)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid chat number"})
-		return
-	}
-	updatedMessage.Number = messageNum
-
-	// Create a queue message with the operation type "update"
-	queueData := queueMessage{
-		Operation: "update", // Set the operation type to update
-		Data:      updatedMessage,
-	}
-
-	// Serialize the queue message to JSON
-	queueMessageData, err := json.Marshal(queueData)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to serialize message data"})
-		return
-	}
-
-	// Push the serialized message data onto a Redis list (queue)
-	ctx := context.Background()
-	err = redis_client.RPush(ctx, "test_messages_queue", queueMessageData).Err()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to push message to Redis queue"})
-		return
-	}
-
-	// Return the updated message data immediately
-	c.JSON(http.StatusOK, updatedMessage)
-}
-func deleteMessage(c *gin.Context) {
-	// Get the application_token, chat_number, and message_number from the URL parameters
-	applicationToken := c.Param("application_token")
-	chatNumber := c.Param("chat_number")
-	messageNumber := c.Param("message_number")
-
-	// Create a message struct using the provided parameters
-	var messageToDelete message
-	messageToDelete.ApplicationToken = applicationToken
-
-	// Convert chat_number to int
-	chatNum, err := strconv.Atoi(chatNumber)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid chat number"})
-		return
-	}
-	messageToDelete.ChatNumber = chatNum
-
-	// Convert message_number to int
-	messageNum, err := strconv.Atoi(messageNumber)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid message number"})
-		return
-	}
-	messageToDelete.Number = messageNum
-
-	// Create a queue message with the operation type "delete"
-	queueData := queueMessage{
-		Operation: "delete", // Set the operation type to delete
-		Data:      messageToDelete,
-	}
-
-	// Serialize the queue message to JSON
-	queueMessageData, err := json.Marshal(queueData)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to serialize message data"})
-		return
-	}
-
-	// Push the serialized message data onto a Redis list (queue)
-	ctx := context.Background()
-	err = redis_client.RPush(ctx, "test_messages_queue", queueMessageData).Err()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to push message to Redis queue"})
-		return
-	}
-
-	// Return a confirmation message
-	c.JSON(http.StatusOK, gin.H{"status": "Message delete request added to queue"})
 }
 
 func main() {
@@ -374,32 +157,13 @@ func main() {
 		DB:       0,  // use default DB
 	})
 
-	DB_HOST := os.Getenv("DB_HOST")
-	DB_PORT := os.Getenv("DB_PORT")
-	DB_USER := os.Getenv("DB_USER")
-	DB_PASSWORD := os.Getenv("DB_PASSWORD")
-	DB_NAME := os.Getenv("DB_NAME")
-	var err error
-	// Connect Database Server
-	db, err = sql.Open("mysql", DB_USER+":"+DB_PASSWORD+"@tcp("+DB_HOST+":"+DB_PORT+")/"+DB_NAME)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-
 	router := gin.Default()
 
 	// Chat routes
-	router.GET("/chats", getChats)
 	router.POST("/chats", createChat)
-	router.PATCH("/chats/:application_token/:chat_number", updateChat)
-	router.DELETE("/chats/:application_token/:chat_number", deleteChat)
 
 	// Message routes
-	router.GET("/messages", getMessages)
 	router.POST("/messages", createMessage)
-	router.PATCH("/messages/:application_token/:chat_number/:message_number", updateMessage)
-	router.DELETE("/messages/:application_token/:chat_number/:message_number", deleteMessage)
 
 	router.Run("0.0.0.0:8080")
 
