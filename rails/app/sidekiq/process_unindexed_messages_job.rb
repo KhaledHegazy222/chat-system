7c15d83db472d77f55769d7f2efe81c7aad65506
KhaledHegazy222 <khaledhegazy870@gmail.com>
2024-10-07 18:16:13 +0300

    add bulk import in elastic search

--- rails/app/sidekiq/process_unindexed_messages_job.rb
+++ rails/app/sidekiq/process_unindexed_messages_job.rb
@@ -6,11 +6,17 @@ class ProcessUnindexedMessagesJob
     puts "Proecessing Unindexed Messages......"  
     ActiveRecord::Base.transaction do
         @messages = Message.all.where(es_indexed: false)
-        @messages.each do |message|
-          # Indexing Each Document Individually
-          # TODO: Bulk Index for all of these messages
-          message.__elasticsearch__.index_document
-        end
+        # Bulk Index for all of these messages
+        bulk_body = @messages.map do |message|
+          [
+            { index: { _id: message.id, _index: Message.index_name } }, 
+            message.as_json
+          ]
+        end.flatten
+        Message.__elasticsearch__.client.bulk({
+          index: Message.index_name,
+          body: bulk_body
+        })
     
         # Update es_indexed to true for all processed messages
         Message.where(es_indexed: false).update_all(es_indexed: true)
